3448331950a8a350107fe497c6067ee640bc1e22
matheus_santos_fraga <matheus_santos_fraga@SNSOO1EP01VM119.saojose.sc.senai.br>
2015-10-19 21:37:29 -0200

    Alteração de método List - de JDBC para Hibernate

--- TCCGrupo8/src/java/dao/UsuarioDao.java
+++ TCCGrupo8/src/java/dao/UsuarioDao.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 public class UsuarioDao {
@@ -14,9 +15,9 @@ public class UsuarioDao {
     public void saveOrUpdate(Usuario usuario) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.getTransaction().begin();
-        
+
         session.saveOrUpdate(usuario);
-        
+
         session.getTransaction().commit();
         session.close();
     }
@@ -24,7 +25,7 @@ public class UsuarioDao {
     public void excluir(Usuario usuario) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.getTransaction().begin();
-        
+
         session.delete(usuario);
 
         session.getTransaction().commit();
@@ -35,45 +36,27 @@ public class UsuarioDao {
         Usuario usuario;
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.getTransaction().begin();
-        
+
         usuario = (Usuario) session.get(Usuario.class, id); //Classe, Id
-        
+
         session.getTransaction().commit();
         session.close();
-        
+
         return usuario;
     }
+
     public List<Usuario> listar() {
-        List<Usuario> lista = new ArrayList<Usuario>();
-        try {
-            String QUERY_DETALHE = "select * from USUARIO";
-            PreparedStatement stmt = null;
-            Connection conn = ConnectionManager.getConnection();
+        List<Usuario> usuarioList;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.getTransaction().begin();
+        
+        Query query = session.createQuery("from entity.Usuario");
+        usuarioList = query.list();
+        
+        session.getTransaction().commit();
+        session.clear();
 
-            ResultSet rs = null;
-
-            stmt = conn.prepareStatement(QUERY_DETALHE);
-            rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Usuario usuario = new Usuario();
-                usuario.setId(rs.getInt("idUsuario"));
-                usuario.setNome(rs.getString("nome"));
-                usuario.setEmail(rs.getString("email"));
-                usuario.setSenha(rs.getString("senha"));
-                lista.add(usuario);
-            }
-            conn.close();
-
-        } catch (Exception ex) {
-            
-            ex.printStackTrace();
-            
-        } finally {
-            
-            return lista;
-            
-        }
+        return usuarioList;
     }
 
 }
